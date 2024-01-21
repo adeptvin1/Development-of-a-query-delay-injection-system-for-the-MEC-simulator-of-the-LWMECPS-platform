39867aff766c6778a479fb985a288308762c1205
progML <matveylesonen@gmail.com>
2024-01-21 19:02:34 +0300

    change pod-scaling.py

--- mec-orch-app/pod-scaling.py
+++ mec-orch-app/pod-scaling.py
@@ -38,4 +38,4 @@ if __name__ == "__main__":
     import uvicorn
 
     # Run the FastAPI application using Uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
